396b0aebec97fccd8dae703216ac05a6e99f5a23
Kelvin Mwinuka <kelvin@Kelvins-MacBook-Pro.local>
2022-03-11 23:50:47 +0300

    Added filter for request method on api routes

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,7 @@
 
 export default function handler(req, res) {
+	if (!['POST'].includes(req.method)) {
+		return res.status(400).json({ message: 'Method not allowed' })
+	}
 	res.status(200).json({ name: 'John Doe' })
 }
